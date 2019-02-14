6effe316625a7a44dd2632ccdb1cabd2d3161f0e
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-02-14 14:15:26 -0500

    consolidated azure functions into one file so it deploys easier

--- Memebot.Functions/Memebot.Functions.cs
+++ Memebot.Functions/Memebot.Functions.cs
@@ -1,11 +1,30 @@
 using System;
-using Memebot.Library;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Memebot.Library;
 
-namespace Memebot.Collector
+namespace Memebot.Functions
 {
+    public static class PostMemesToSlack
+    {
+        [FunctionName("PostMemesToSlack")]
+        public static void Run([TimerTrigger("0 0 */4 * * *")]TimerInfo myTimer, ILogger log)
+        {
+            // log into KeyVault and get secrets
+            KeyVaultHelper.LogIntoKeyVault();
+            var storageConnectionString = KeyVaultHelper.GetSecret("https://memebot-keyvault.vault.azure.net/secrets/storage-connection-string/");
+            var slackWebhookUrl = KeyVaultHelper.GetSecret("https://memebot-keyvault.vault.azure.net/secrets/slack-webhook-url/");
+
+
+            // post memes from Azure Queue Storage into Slack
+            int numMemes = 2;
+            Library.Memebot.PostToSlackFromQueue(numMemes, slackWebhookUrl, storageConnectionString);
+            
+            // logs back to Azure
+            log.LogInformation($"Posted {numMemes} to Slack at: {DateTime.Now}");
+        }
+    }
     public static class MemebotCollector
     {
         [FunctionName("MemebotCollector")]
