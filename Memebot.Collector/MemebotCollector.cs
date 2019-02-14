a7f1ec79d5eee39cd128e73f0601e0a75609de1a
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-02-14 13:59:55 -0500

    made collector azure function

---
+++ Memebot.Collector/MemebotCollector.cs
@@ -0,0 +1,40 @@
+using System;
+using Memebot.Library;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Memebot.Collector
+{
+    public static class MemebotCollector
+    {
+        [FunctionName("MemebotCollector")]
+        public static void Run([TimerTrigger("0 0 0 * * *")]TimerInfo myTimer, ILogger log)
+        {
+            // spiciest memes around
+            string[] subredditList = 
+            {
+                "/r/prequelmemes",
+                "/r/4chan",
+                "/r/animemes",
+                "/r/blackpeopletwitter",
+                "/r/bikinibottomtwitter",
+            };
+
+            // log into KeyVault and get secrets
+            KeyVaultHelper.LogIntoKeyVault();
+            var storageConnectionString = KeyVaultHelper.GetSecret("https://memebot-keyvault.vault.azure.net/secrets/storage-connection-string/");
+            var slackWebhookUrl = KeyVaultHelper.GetSecret("https://memebot-keyvault.vault.azure.net/secrets/slack-webhook-url/");
+
+
+            // collect memes and store in Azure Queue Storage
+            var reddit = Library.Memebot.CreateReddit();
+
+            int memesPerSub = 3;
+            Library.Memebot.CollectTopMemesToQueue(memesPerSub, subredditList, reddit, storageConnectionString);
+
+            int totalMemes = memesPerSub * subredditList.Length;
+            log.LogInformation($"Collected {totalMemes} memes and stored into queue at {DateTime.Now}");
+        }
+    }
+}
